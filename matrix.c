6f369c4d0e6c3ee7bd4c003b2547696f56883a97
bool <booleancoercion@gmail.com>
2022-03-01 18:36:41 +0200

    wrote basic matrix arithmetic

---
+++ matrix.c
@@ -0,0 +1,98 @@
+#include "matrix.h"
+#include <stdio.h>
+
+matrix_t matrix_new(size_t rows, size_t cols) {
+    size_t len = rows * cols;
+    double *data = calloc(len, sizeof(double));
+
+    matrix_t output = {.data = data, .rows = rows, .cols = cols, .len = len};
+    return output;
+}
+
+matrix_t matrix_clone(matrix_t mat) {
+    matrix_t cloned = matrix_new(mat.rows, mat.cols);
+    if(NULL == cloned.data) {
+        return cloned; // return early to avoid accessing `data`
+    }
+
+    for(size_t idx = 0; idx < mat.len; idx++) {
+        cloned.data[idx] = mat.data[idx];
+    }
+
+    return cloned;
+}
+
+size_t matrix_calc_index(matrix_t mat, size_t i, size_t j) {
+    if(i >= mat.rows) {
+        printf("invalid index for matrix: the number of rows is %zu but the "
+               "index is %zu\n",
+               mat.rows, i);
+        return (size_t)-1;
+    }
+
+    if(j >= mat.cols) {
+        printf("invalid index for matrix: the number of cols is %zu but the "
+               "index is %zu\n",
+               mat.cols, j);
+        return (size_t)-1;
+    }
+
+    return i * mat.cols + j;
+}
+
+double matrix_get(matrix_t mat, size_t i, size_t j) {
+    size_t index = matrix_calc_index(mat, i, j);
+    return mat.data[index];
+}
+
+int matrix_add_assign(matrix_t self, matrix_t other) {
+    if(!(self.rows == other.rows && self.cols == other.cols)) {
+        return DIM_MISMATCH;
+    }
+
+    for(size_t idx = 0; idx < self.len; idx++) {
+        self.data[idx] += other.data[idx];
+    }
+
+    return 0;
+}
+
+int matrix_add(matrix_t mat1, matrix_t mat2, matrix_t *output) {
+    if(!(mat1.rows == mat2.rows && mat1.cols == mat2.cols)) {
+        return DIM_MISMATCH;
+    }
+
+    *output = matrix_new(mat1.rows, mat1.cols);
+    if(NULL == output->data) {
+        return BAD_ALLOC;
+    }
+
+    for(size_t idx = 0; idx < mat1.len; idx++) {
+        output->data[idx] = mat1.data[idx] + mat2.data[idx];
+    }
+
+    return 0;
+}
+
+void matrix_mul_scalar_assign(matrix_t mat, double scalar) {
+    for(size_t idx = 0; idx < mat.len; idx++) {
+        mat.data[idx] *= scalar;
+    }
+}
+
+matrix_t matrix_mul_scalar(matrix_t mat, double scalar) {
+    matrix_t output = matrix_new(mat.rows, mat.cols);
+    if(NULL == output.data) {
+        return output;
+    }
+
+    for(size_t idx = 0; idx < mat.len; idx++) {
+        output.data[idx] = mat.data[idx] * scalar;
+    }
+
+    return output;
+}
+
+void matrix_free(matrix_t mat) {
+    free(mat.data);
+}
---
+++ matrix.h
@@ -0,0 +1,76 @@
+#ifndef MATRIX_H
+#define MATRIX_H
+#include <stdlib.h>
+
+#define DIM_MISMATCH 1
+#define BAD_ALLOC 2
+
+typedef struct matrix {
+    double *data;
+    size_t rows;
+    size_t cols;
+    size_t len;
+} matrix_t;
+
+/*
+ * BASIC METHODS
+ */
+
+// Creates a new matrix with the given dimensions. The created matrix must be
+// freed with `matrix_free`. The created matrix is zero-initialized.
+//
+// In case of allocation failure, the output matrix has a `data` field of
+// `NULL`.
+matrix_t matrix_new(size_t rows, size_t cols);
+
+// Clones the given matrix into a newly allocated matrix.
+//
+// In case of allocation failure, the output matrix has a `data` field of
+// `NULL`.
+matrix_t matrix_clone(matrix_t mat);
+
+// Calculates the index of the desired element for use with the matrix's
+// inner `data` field. This is mainly used for optimization.
+//
+// This function also performs a bounds check on the given values: in case
+// of failure, an error is printed to the console and the returned value
+// is the maximum for `size_t`.
+size_t matrix_calc_index(matrix_t mat, size_t i, size_t j);
+
+// Gets the desired element from the given matrix.
+//
+// This function calls `matrix_calc_index`.
+double matrix_get(matrix_t mat, size_t i, size_t j);
+
+// Frees a given matrix that was allocated using any matrix method.
+void matrix_free(matrix_t mat);
+
+/*
+ * ARITHMETIC METHODS
+ */
+
+// Adds the second matrix into the first. The return value is `0` only if
+// the dimensions match correctly, and `DIM_MISMATCH` otherwise.
+//
+// In case of failure, the first matrix isn't modified.
+int matrix_add_assign(matrix_t self, matrix_t other);
+
+// Adds the given matrices and stores the newly allocated matrix in `output`.
+// The return value is `0` if and only if the operation was successful.
+//
+// Errors:
+//
+// In case of dimension mismatch, the return value is `DIM_MISMATCH`.
+// In case of allocation failure, the return value is `BAD_ALLOC`.
+int matrix_add(matrix_t mat1, matrix_t mat2, matrix_t *output);
+
+// Multiplies the given scalar into the given matrix.
+void matrix_mul_scalar_assign(matrix_t mat, double scalar);
+
+// Multiplies the given scalar and matrix, and returns the allocated result.
+//
+// In case of allocation failure, the output matrix has a `data` field of
+// `NULL`.
+matrix_t matrix_mul_scalar(matrix_t mat, double scalar);
+
+#endif // MATRIX_H

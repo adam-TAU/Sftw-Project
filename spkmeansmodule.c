2cece92b2ca9dd9aaba08d2065a829f4b928c96b
adam <adam@adam.adam>
2022-03-08 05:25:56 +0200

    CPython interface is fully compatible. now onto the actual implementation of CPython's API

--- spkmeansmodule.c
+++ spkmeansmodule.c
@@ -4,22 +4,41 @@
 
 
 
+/**************************************************************************/
+
+static PyObject* goal(PyObject *self, PyObject *args);
+static PyObject* kmeans(PyObject *self, PyObject *args);
+
+
+/**************************************************************************/
+
+
+
+static PyObject* goal(PyObject *self, PyObject *args) {
+	Py_RETURN_NONE;
+}
 
 
 
 
-
-
+static PyObject* kmeans(PyObject *self, PyObject *args) {
+	Py_RETURN_NONE;
+}
 
 
 /**************************************************************************/
 
 
 static PyMethodDef capiMethods[] = {
-        {"fit",
-                (PyCFunction) fit_capi,
+        {"goal",
+                (PyCFunction) goal,
                 METH_VARARGS,
-                PyDoc_STR("TBD")
+                PyDoc_STR("Perform the wanted operations on the given datapoints, corresponding to the determined 'goal'")
+        },
+        {"kmeans",
+                (PyCFunction) kmeans,
+                METH_VARARGS,
+                PyDoc_STR("Given a set of datapoints, an array of the indices of the initial centroids (induced from kmeans++'s first step), perform the kmeans algorithm")
         },
         {NULL, NULL, 0, NULL}
 };

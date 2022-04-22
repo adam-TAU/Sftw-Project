01e01bddb53f72a0491adb9526096913e6aac2f9
bool <booleancoercion@gmail.com>
2022-04-22 13:51:03 +0300

    cleaned up compilation script according to instructions

--- 215334822_325844611_final/comp.sh
+++ 215334822_325844611_final/comp.sh
@@ -1,13 +1,4 @@
 #!/bin/bash
 
 # assembling and linking
-gcc -ansi -Wall -Wextra -Werror -pedantic-errors -c matrix.c -lm -o matrix.o
-gcc -ansi -Wall -Wextra -Werror -pedantic-errors -c graph.c -lm -o graph.o
-gcc -ansi -Wall -Wextra -Werror -pedantic-errors -c eigen.c -lm -o eigen.o
-gcc -ansi -Wall -Wextra -Werror -pedantic-errors -c spkmeans.c -lm -o spkmeans.o
-gcc -ansi -Wall -Wextra -Werror -pedantic-errors -c spkmeans_goals.c  -lm -o spkmeans_goals.o
-gcc -ansi -Wall -Wextra -Werror -pedantic-errors matrix.o graph.o eigen.o spkmeans.o spkmeans_goals.o -lm -o spkmeans
-
-
-# clean up
-rm *.o
+gcc -ansi -Wall -Wextra -Werror -pedantic-errors matrix.c graph.c eigen.c spkmeans.c spkmeans_goals.c -lm -o spkmeans
